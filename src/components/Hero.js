e0e01e7194a990c2b8a1c934a50b1f9aa0b182aa
Mahmud Ghali <mahmudghali01@gmail.com>
2022-06-12 16:03:03 +0100

    Initial commit

---
+++ src/components/Hero.js
@@ -0,0 +1,34 @@
+export default () => {
+      return (
+          <div className="bg-emerald-400">
+              <section className="mt-0 mx-auto max-w-screen-xl pb-12 px-4 items-center lg:flex md:px-8">
+                  <div className="space-y-4 flex-1  sm:text-center lg:text-left">
+                  <div className="items-center  mx-auto max-w-screen-xl sm:px-8 sm:flex sm:space-x-6">
+                      <img src="https://i.postimg.cc/XqfvHj1J/20220612-002710.jpg" alt="my picture" className="box-border h-32 w-32 p-4  rounded-full"></img>
+                  </div>
+                      <h1 className="text-white font-bold text-4xl xl:text-5xl">
+                          Hello There,
+                      <p><span className="text-green-900"> I am Haleem</span></p>     
+                      </h1>
+                      <p className="text-teal-900 max-w-xl leading-relaxed sm:mx-auto lg:ml-0 font-mono font-bold">
+                          I am a Full-stack web developer, I know HTML, CSS and JavaScript(Not a fan but gotta use it).
+                         <p>I also use Python(Django) For Backend</p> 
+                          and my favorite Database is Postgresql
+
+
+                      </p>
+                      <div className="pt-10 items-center justify-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex lg:justify-start">
+                          <a href="javascript:void(0)" className="px-7 py-3 w-full bg-white text-gray-800 text-center rounded-md shadow-md block sm:w-auto">
+                              Get started
+                          </a>
+                          
+                      </div>
+                  </div>
+                  <div className="flex-1 text-center mt-4 lg:mt-0 lg:ml-3">
+                      <img src="https://i.postimg.cc/xT6jp4T9/browser-website-pngrepo-com.png" className="w-full mx-auto sm:w-10/12  lg:w-full" />
+                  </div>
+              </section>
+          </div>
+      )
+  }
+  
